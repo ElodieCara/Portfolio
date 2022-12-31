821c1a96e9f97743e088916eb744d7cceb7b5351
ElodieCara <102527658+ElodieCara@users.noreply.github.com>
2022-12-31 20:55:07 +0100

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { HiArrowNarrowRight } from 'react-icons/hi'
+
+const Home = () => {
+    return (
+        <div className='w-full h-screen flex items-center justify-center h-64  
+        bg-gradient-to-r 
+        from-blue-300 
+        to-orange-100 
+        via-purple-200
+        animate-gradient-x'>
+
+        </div>
+    );
+};
+
+export default Home;
